fd3518811b3924586df67509d9aeb9b1bcb861ac
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-05-05 00:01:05 +0800

    Welcoming the user and ask to input an option for math operations

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,6 +1,15 @@
 # pseudocode for simple calculator
 
 # Ask user to choose from four math operations
+print("Welcome to Simple Calculator App! \n"
+      "Let's do some math! :D")
+print("What would you like to do? \n"
+      " 1. Addition (+) \n"
+      " 2. Subtraction (-) \n"
+      " 3. Multiplication (×) \n"
+      " 4. Division (÷)")
+chosen_option = int(input("Choose an option: "))
+
 # Ask user to input two numbers
 # Display the result
 # Ask user again if they want to try again
