07ab843cedf79985f2dc40f25e90b2dd5ecff8b7
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-05-04 23:49:23 +0800

    Added pseudocode

---
+++ simple_calculator.py
@@ -0,0 +1,8 @@
+# pseudocode for simple calculator
+
+# Ask user to choose from four math operations
+# Ask user to input two numbers
+# Display the result
+# Ask user again if they want to try again
+# If yes, repeat the process
+# If no, display "Thank you" and exit the program.
