bc7df715c41906c04d2aaff87b42b19b48606dde
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-05-05 19:28:36 +0800

    Enhanced formatting with '>>>' to improve visual clarity and as input prompts for user guidance

--- simple_calculator.py
+++ simple_calculator.py
@@ -13,7 +13,7 @@ def welcome_message():
 # Ask user to choose from four math operations
 welcome_message()
 while True:
-    print("What would you like to do? \n"
+    print(">>> What would you like to do? \n"
           " 1. Addition (+) \n"
           " 2. Subtraction (-) \n"
           " 3. Multiplication (×) \n"
@@ -21,12 +21,12 @@ while True:
           " 5. Exit ")
 
     try:
-        chosen_option = int(input("Choose an option: "))
+        chosen_option = int(input(">>> Choose an option: "))
         if chosen_option not in range(1, 6):  # Validation if user enters other numbers
             print("Invalid choice. Please enter a number between 1 and 4")
             continue
         if chosen_option == 5:
-            print("Thank you for using Simple Calculator App! :D")
+            print(" Thank you for using Simple Calculator App! :D")
             break
 
     except ValueError:  # If a user enters other characters
@@ -35,8 +35,8 @@ while True:
 
     # Ask user to input two numbers
     try:
-        first_number = int(input("Enter your first number: "))
-        second_number = int(input("Enter your second number: "))
+        first_number = int(input(">>> Enter your first number: "))
+        second_number = int(input(">>> Enter your second number: "))
     except ValueError:
         print("Invalid input. Please enter numeric values only.")
         continue
@@ -56,10 +56,10 @@ while True:
             continue
 
     # Ask user again if they want to try again
-    print("Do you want to continue? \n"
+    print(">>> Do you want to continue? \n"
           " (a) Yes      (b) No")
     try:
-        try_again = str(input("Choose an option (a/b): "))
+        try_again = str(input(">>> Choose an option (a/b): "))
 
         # If yes, repeat the process
         if try_again == 'a':
