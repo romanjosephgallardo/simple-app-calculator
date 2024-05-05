2564ab88b78e2f3e966d2f9d41f646cd6dd55648
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-05-05 10:41:00 +0800

    Updated exit( ) calls to 'continue' to improved loop control

--- simple_calculator.py
+++ simple_calculator.py
@@ -14,10 +14,11 @@ while True:
         chosen_option = int(input("Choose an option: "))
         if chosen_option not in range(1, 5):  # Validation if user enters other numbers
             print("Invalid choice. Please enter a number between 1 and 4")
+            continue
 
     except ValueError:  # If a user enters other characters
         print("Invalid choice. Please enter a valid number")
-        exit()
+        continue
 
     # Ask user to input two numbers
     try:
@@ -25,6 +26,7 @@ while True:
         second_number = int(input("Enter your second number: "))
     except ValueError:
         print("Invalid input. Please enter numeric values")
+        continue
 
     # Display the result
     if chosen_option == 1:
@@ -38,6 +40,7 @@ while True:
             print(f"The quotient is: {first_number / second_number}")
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
+        continue
 
     # Ask user again if they want to try again
     print("Do you want to continue? \n"
@@ -47,12 +50,12 @@ while True:
 
         # If yes, repeat the process
         if try_again == 'a':
-            exit()  # will change this part for looping
+            continue
         # If no, display "Thank you" and exit the program.
         elif try_again == 'b':
             print("Thank you for using Simple Calculator App! :D")
         if try_again not in ('a', 'b'):
             print("Invalid choice. Please enter a valid letter")
-            exit()
+            continue
     except:
         exit()
